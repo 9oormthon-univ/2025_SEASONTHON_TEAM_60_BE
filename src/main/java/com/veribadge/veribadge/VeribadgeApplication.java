329c0bd91ed03925bb3ef719dba2e09a6a5bba10
2unhy2 <144293456+2unhy2@users.noreply.github.com>
2025-08-30 12:26:04 +0900

    feat: Initial project setup with swagger and db config

---
+++ src/main/java/com/veribadge/veribadge/VeribadgeApplication.java
@@ -0,0 +1,13 @@
+package com.veribadge.veribadge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeribadgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeribadgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veribadge/veribadge/VeribadgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veribadge.veribadge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeribadgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
